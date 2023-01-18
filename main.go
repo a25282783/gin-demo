fe2b2b6caeea7d96ffd4b909b35a3bfb2a25b9f3
Louis Liu <lliu@elta.com.tw>
2023-01-18 17:53:30 +0800

    practice

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	// 加載資源
 	r.Static("/public", "./public")
 	// 請求參數
-	r.GET("/user/info", func(ctx *gin.Context) {
+	r.GET("/index", func(ctx *gin.Context) {
 		userid := ctx.Query("id")
 		usersex := ctx.Query("sex")
 		ctx.HTML(http.StatusOK, "index.html", gin.H{
@@ -22,6 +22,32 @@ func main() {
 			"usersex": usersex,
 		})
 	})
+	// 綁定參數並返回json
+	r.GET("/user/:id/:name", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		name := ctx.Param("name")
+		ctx.JSON(http.StatusOK, gin.H{
+			"id":   id,
+			"name": name,
+		})
+	})
+	// 表單
+	r.POST("/create/post", func(ctx *gin.Context) {
+		t := ctx.DefaultPostForm("title", "default title")
+		a := ctx.DefaultPostForm("article", "default article")
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"t": t,
+			"a": a,
+		})
+	})
+	// 404
+	r.NoRoute(func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "404.html", "")
+	})
+	// redirect
+	r.GET("/redirect", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusFound, "http://localhost:8008/index")
+	})
 
-	r.Run(":8005") // listen and serve on 0.0.0.0:8080
+	r.Run(":8008") // listen and serve on 0.0.0.0:8080
 }
